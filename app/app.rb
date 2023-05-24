10b71afd5d72cc297a225e6e996e98c4f6ef70c2
Micheal Waltz <micheal@ecliptik.com>
2023-05-24 07:36:45 -0700

    update comment

--- app/app.rb
+++ app/app.rb
@@ -4,7 +4,7 @@ require "erb"
 #Disable exceptions
 disable :show_exceptions
 
-#Single route to call update_file method and print result
+#Return env vars
 get "/environment" do
   "foo: #{ENV['AWS_KEY_ID']}\nexcited: #{ENV['AWS_SECRET_KEY_ID']}"
 end
