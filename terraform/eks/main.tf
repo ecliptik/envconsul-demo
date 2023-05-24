eb8ac776679d227666c2bd750d9bebb2fcbd9ad8
Micheal Waltz <micheal@ecliptik.com>
2023-05-23 21:51:55 -0700

    Use eks 1.22, amazon linux 2, add SSM managed policy

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -40,17 +40,20 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.13.1"
+  version = "19.14.0"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.26"
+  cluster_version = var.eks_version
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_ARM_64"
+    ami_type = var.ami_type
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
   }
 
   eks_managed_node_groups = {
--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -6,3 +6,13 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "ami_type" {
+  description = "AMI type to use"
+  default     = "AL2_ARM_64"
+}
+
+variable "eks_version" {
+  description = "EKS Version"
+  default     = "1.22"
+}
